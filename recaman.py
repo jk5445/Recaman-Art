2742705a4fe42fdd4bc053ce0c49eaece3dca8cc
jk5445 <judah.kishk@gmail.com>
2021-09-13 15:10:11 -0400

    repeat on left side for symmetry

--- recaman.py
+++ recaman.py
@@ -26,4 +26,25 @@ for step in range(200):
     current += sign*step
     seen.add(current)
 
+
+# Left Side
+joe = turtle.Turtle()
+joe.speed(0)
+
+seen.clear()
+current = 0
+
+for step in range(200):
+    sign = 1
+    joe.pencolor("black")
+
+    if current - step > 0 and current - step not in seen:
+        sign = -1
+
+    joe.setheading(sign*90)
+    joe.circle(scale*step/2, -180)
+
+    current += sign*step
+    seen.add(current)
+
 turtle.done()
