d45288ee0a3ec2221d277e8c3f0f2af172555338
jk5445 <judah.kishk@gmail.com>
2021-09-13 17:24:26 -0400

    Update recaman_graphics.py to take colors as command line arguments

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ In this project I will introduce myself to some Python graphics tools using the 
 ## Recaman Sequence
 The recaman sequence is a defined by a recurrence relation that begins at 0. a[n] = a[n-1] - n if a[n-1] - n > 0 and a[n-1] - n is not already in the sequence. Otherwise, a[n] = a[n-1] + n.
 
+## recaman_graphics
+Implemented with Python's graphics package.
+
+### Usage
+Optional command line arguments:
+
+Usage: recaman_graphics.py --outline=\<outlinecolor\> --background=\<backgroundcolor\>
+
+Default colors are outline = antiquewhite1 and background = lightskyblue
+
+Program closes when user clicks anywhere in the window.
+
+### Method
+Draw circles with radius n/2.
+Circles are centered between a[n] and a[n-1].
+
 ## recaman_turtle
 Implemented with Python's turtle package.
 
@@ -11,15 +27,3 @@ Implemented with Python's turtle package.
 Draw semi-circles with radius n/2.
 Semi-circles have alternating orientation and face either up or down.
 Semi-cirlces begin from the endpoint of the previous semi-circle, a[n-1], and end at a[n]
-
-## recaman_graphics
-
-Implemented with Python's graphics package.
-
-Program closes when user clicks anywhere in the window.
-
-### Method
-
-Draw circles with radius n/2.
-Circles are centered between a[n] and a[n-1].
-
--- recaman_graphics.py
+++ recaman_graphics.py
@@ -1,10 +1,49 @@
 from graphics import GraphWin, update, Circle, Point
+import sys
+import tkinter
 
 outline = 'antiquewhite1'
 background = 'lightskyblue'
 
+user_outline = ''
+user_background = ''
+argv = sys.argv[1:]
+
+# Read command line arguments
+for i in range(len(argv) - 1):
+	if argv[i] in ("-o", "--outline"):
+		user_outline = argv[i+1]
+	if argv[i] in ("-b", "--background"):
+		user_background = argv[i+1]
+
+	if argv[i] in ("-h", "--Help"):
+		print("Usage: recaman_graphics.py --outline=<outlinecolor> --background=<backgroundcolor>")
+		print("Default colors are outline = antiquewhite1 and background = lightskyblue")
+
+if len(argv) and argv[-1] in ("-h", "--Help"):
+	print("Usage: recaman_graphics.py --outline=<outlinecolor> --background=<backgroundcolor>")
+	print("Default colors are outline = antiquewhite1 and background = lightskyblue")
+
+# Instanciate GraphWin
 win = GraphWin("Recamán – Click Anywhere to Close", 1400, 800, autoflush=False)
 win.setCoords(-win.getWidth()/2,-win.getHeight()/2,win.getWidth()/2,win.getHeight()/2)
+
+# Test user input for validity
+if user_outline:
+	try:
+		win.setBackground(user_outline)
+		outline = user_outline
+	except tkinter.TclError:
+		print("Outline: " + user_outline + " is not a valid color name. Please enter a valid X11 color name.")
+if user_background:
+	try:
+		win.setBackground(user_background)
+		background = user_background
+	except tkinter.TclError:
+		print("Backround: " + user_background + " is not a valid color name. Please enter a valid X11 color name.")
+print("Rendering with", outline, "outline and", background, "background")
+
+# Render
 win.setBackground(background)
 
 seen = set()
@@ -15,21 +54,21 @@ scale = 2
 for step in range (500):
 	sign = 1
 
-	#determine direction
+	# Determine direction
 	if current - step > 0 and current - step not in seen:
 		sign = -1
 
-	#right side
+	# Right side
 	c = Circle(Point(scale*(2*current+sign*step)/2 , 0), scale*step/2)
 	c.setOutline(outline)
 	c.draw(win)
 
-	#left side
+	# Left side
 	c = Circle(Point(-scale*(2*current+sign*step)/2 , 0), scale*step/2)
 	c.setOutline(outline)
 	c.draw(win)
 
-	#update current
+	# Update current
 	current += sign*step
 	seen.add(current)
 
@@ -37,3 +76,5 @@ update()
 
 win.getMouse()
 win.close()
+
+print("Window closed")
